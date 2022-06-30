2a2aeff9a010097b3ed45d7e0908fbd1d46bd919
Muhammad Faisal <mfessal475@gmail.com>
2022-06-30 15:27:09 +0500

    final commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="card">
+
+      <img className="card-hero-image" src="./assets/laurasmith.png" alt="twitter"/>
+
+      <div className="body">
+        <h1 className="title">Laura Smith</h1>
+        <p className="designation">Frontend Developer</p>
+        <p className="weblink">laurasmith.website</p>
+
+        <button className="button-left" href=""><img src="./assets/email.svg" />Email</button>
+        <button className="button-right"><img src="./assets/linkedin.svg" />LinkedIn</button>
+        
+        <h2 className="text-heading text-heading-1">About</h2>
+        <p className="text text-1">I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
+        <h2 className="text-heading text-heading-2">Interests</h2>
+        <p className="text text-2">Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
+      </div>
+
+      <footer className="footer">
+          <img className="footer-icons" src="./assets/twitter.svg" alt="twitter"/>
+          <img className="footer-icons" src="./assets/facebook.svg" alt="facebook"/>
+          <img className="footer-icons" src="./assets/instagram.svg" alt="instagram"/>
+          <img className="footer-icons" src="./assets/github.svg" alt="github"/>
+      </footer>
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
